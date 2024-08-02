83cb4fb2505d7058550270fa2f0a118d54bde844
Mouli Bheemaneti <moulibheemaneti99@gmail.com>
2024-08-02 08:09:27 +0530

    Added a dummy community details screen.

---
+++ pages/communities/[id].vue
@@ -0,0 +1,22 @@
+<template>
+    <main class="page-community">
+        D
+        <i class="fa fa-wifi"></i>
+        <i class="fa fa-bedroom"></i>
+    </main>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss">
+@use '@/assets/scss/abstracts/index.scss' as *;
+
+.page-community {
+    padding: 1rem;
+
+    @include min-tablet {
+        padding: calc($padding-desktop/2) $padding-desktop;
+    }
+}
+</style>
